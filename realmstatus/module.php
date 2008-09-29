8cec806f9d35c6250eb533960d73bdb39a5d5386
osr-corgan <osr-corgan@eqdkp-plus.eu>
2008-09-29 21:56:45 +0000

    Add Lotro Support to Realmstatus Script

--- realmstatus/module.php
+++ realmstatus/module.php
@@ -2,7 +2,7 @@
 /*
  * Project:     EQdkp-Plus
  * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * Link:        http://creativecommons.org/licenses/by-nc-sa/3.0/
  * -----------------------------------------------------------------------
  * Began:       2008
  * Date:        $Date: $
@@ -12,32 +12,33 @@
  * @link        http://eqdkp-plus.com
  * @package     eqdkp-plus
  * @version     $Rev:  $
- * 
+ *
  * $Id: $
  */
 
-if ( !defined('EQDKP_INC') ){
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC'))
+{
+  header('HTTP/1.0 404 Not Found');exit;
 }
+
 
 // You have to define the Module Information
 $portal_module['realmstatus'] = array(                     // the same name as the folder!
-			'name'			    => 'Realmstatus Module',             // The name to show
-			'path'			    => 'realmstatus',                    // Folder name again
-			'version'		    => '0.0.1',                          // Version
-			'author'        	=> 'Aderyn',                         // Author
-			'contact'		    => 'Aderyn@gmx.net',                 // email adress
-			'description'   	=> 'Show Realmstatus',               // Detailed Description
-			'positions'     	=> array('left1', 'left2', 'right'), // Which blocks should be usable? left1 (over menu), left2 (under menu), right, middle
-      		'signedin'      	=> '0',                              // 0 = all users, 1 = signed in only
-      		'install'       	=> array(
-				                           'autoenable'        => '0',
-				                           'defaultposition'   => 'right',
-				                           'defaultnumber'     => '5', ),				                          
-			);
+      'name'          => 'Realmstatus Module',             // The name to show
+      'path'          => 'realmstatus',                    // Folder name again
+      'version'       => '1.0.0',                          // Version
+      'author'        => 'Aderyn',                         // Author
+      'contact'       => 'Aderyn@gmx.net',                 // email adress
+      'description'   => 'Show Realmstatus',               // Detailed Description
+      'positions'     => array('left1', 'left2', 'right'), // Which blocks should be usable? left1 (over menu), left2 (under menu), right, middle
+      'signedin'      => '0',                              // 0 = all users, 1 = signed in only
+      'install'       => array('autoenable'        => '0',
+                               'defaultposition'   => 'right',
+                               'defaultnumber'     => '5',
+                         ),
+      );
 
 /* Define the Settings if needed
-
 name:       The name of the Database field & Input name
 language:   The name of the language string in the language file
 property:   What type of field? (text,checkbox,dropdown)
@@ -48,34 +49,35 @@ There could be unlimited amount of settings
 Settings page is created dynamically
 */
 $portal_settings['realmstatus'] = array(
-	'pk_realmstatus_realm'	=> array(
+  'pk_realmstatus_realm'  => array(
         'name'      => 'rs_realm',
         'language'  => 'rs_realm',
         'property'  => 'text',
         'size'      => '40',
-	),
-	'pk_realmstatus_us'	=> array(
-          'name'      => 'rs_us',
-          'language'  => 'rs_us',
-          'property'  => 'checkbox',
-	),
+  ),
+  'pk_realmstatus_us'     => array(
+        'name'      => 'rs_us',
+        'language'  => 'rs_us',
+        'property'  => 'checkbox',
+  ),
 );
-if(extension_loaded('gd') && function_exists('gd_info')) {
-	$portal_settings['realmstatus']['pk_realmstatus_gd'] = array(
-          'name'      => 'rs_gd',
-          'language'  => 'rs_gd',
-          'property'  => 'checkbox',
-          'text'      => 'GD LIB Version',
-      );
+
+if(extension_loaded('gd') && function_exists('gd_info'))
+{
+  $portal_settings['realmstatus']['pk_realmstatus_gd'] = array(
+        'name'      => 'rs_gd',
+        'language'  => 'rs_gd',
+        'property'  => 'checkbox',
+        'text'      => 'GD LIB Version',
+  );
 }
 
 // The output function
-// the name MUST be FOLDERNAME_module, if not an error will occur
 if(!function_exists(realmstatus_module))
 {
   function realmstatus_module()
   {
-  	global $plang, $conf_plus, $eqdkp_root_path;
+    global $plang, $conf_plus, $eqdkp_root_path, $eqdkp;
 
     $realmnames = array();
     // list of realms by portal modul config?
@@ -89,37 +91,23 @@ if(!function_exists(realmstatus_module))
     {
       $realmnames[] = $conf_plus['pk_servername'];
     }
-    
-     // output realms
+
+    // output realms
     $realmstatus = '';
     if (count($realmnames) > 0 && strlen($realmnames[0]))
     {
-      $realmstatus .= '<table width="100%" border="0" cellspacing="1" cellpadding="2" class="noborder">';
-        if ($conf_plus['rs_gd']) {
-            $region = ($conf_plus['rs_us']) ? 'us' : 'eu';
-            foreach ($realmnames as $realmname) 
-             {
-                $realmname = trim($realmname);
-                $realmstatus .= '<tr><td align="center">';
-                $realmstatus .= '<img alt="WoW Server Status" src="'.$eqdkp_root_path.'portal/realmstatus/wow_ss.php?realm='.$realmname.'&amp;region='.$region.'"/>';
-                $realmstatus .=  '</td></tr>';
-             }  
-        }
-        else {
-            foreach ($realmnames as $realmname)
-             {
-              	$region			= ($conf_plus['rs_us']) ? '1' : '2';
-                $realmname		= trim($realmname);
-                $replace		= array(" " => "_", "'" => "");
-                $scored_realm		= strtolower(strtr($realmname, $replace));
-                $realmstatus		.= '<tr><td align="center">
-                           <img src="http://www.wowrealmstatus.net/status.php?s='.$scored_realm.'&r='.$region.'"
-                              alt="WoW-Serverstatus: '.$realmname.'"
-                              title="'.$realmname.'"/>
-                           </td></tr>';
-             }
-       }
-          $realmstatus .= '</table>';
+      $status_file = $eqdkp_root_path.'portal/realmstatus/'.$eqdkp->config['default_game'].'/status.php';
+
+      if (file_exists($status_file))
+      {
+        $realmstatus .= '<table width="100%" border="0" cellspacing="1" cellpadding="2" class="noborder">';
+        include($status_file);
+        $realmstatus .= '</table>';
+      }
+      else
+      {
+        $realmstatus .= '<div align="center">'.$plang['rs_game_not_supported'].'</div>';
+      }
     }
     else
     {
@@ -127,7 +115,7 @@ if(!function_exists(realmstatus_module))
     }
 
     // return the output for module manager
-                return $realmstatus;
+    return $realmstatus;
   }
 }
 ?>
