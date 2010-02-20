1f18cbe6a920534887cb2a8cbfaf43399e75a3c4
osr-corgan <osr-corgan@eqdkp-plus.eu>
2010-02-20 15:52:50 +0000

    get realm status from portal modul as text without html

--- realmstatus/WoW/status.php
+++ realmstatus/WoW/status.php
@@ -15,30 +15,38 @@
  *
  * $Id$
  */
-
+  
 if ($conf_plus['rs_gd'])
 {
+	global $pcache;
   $region = ($conf_plus['rs_us']) ? 'us' : 'eu';
   foreach ($realmnames as $realmname)
   {
     $realmname = trim($realmname);
+    $url     	  = $eqdkp_root_path.'portal/realmstatus/WoW/wow_ss.php?realm='.$realmname.'&amp;region='.$region ;
+    $url2     	  = $pcache->BuildLink().'portal/realmstatus/WoW/wow_ss.php?realm='.$realmname.'&amp;region='.$region ;
+    $urls[]		  = $url2;        
     $realmstatus .= '<tr><td align="center">
-                      <img src="'.$eqdkp_root_path.'portal/realmstatus/WoW/wow_ss.php?realm='.$realmname.'&amp;region='.$region.'" alt="WoW-Serverstatus: '.$realmname.'" title="'.$realmname.'"/>
+                      <img src="'.$url.'" alt="WoW-Serverstatus: '.$realmname.'" title="'.$realmname.'"/>
                      </td></tr>';
   }
 }
 else
 {
+  $urls = array();
   foreach ($realmnames as $realmname)
   {
     $region       = ($conf_plus['rs_us']) ? '1' : '2';
     $realmname    = trim($realmname);
     $replace      = array(" " => "_", "'" => "");
     $scored_realm = strtolower(strtr($realmname, $replace));
+    $url     	  = 'http://www.wowrealmstatus.net/status.php?s='.$scored_realm.'&r='.$region ;
+    $urls[]		  = $url;    
     $realmstatus .= '<tr><td align="center">
-                       <img src="http://www.wowrealmstatus.net/status.php?s='.$scored_realm.'&r='.$region.'" alt="WoW-Serverstatus: '.$realmname.'" title="'.$realmname.'"/>
+                      <img src="'.$url.'" alt="WoW-Serverstatus: '.$realmname.'" title="'.$realmname.'"/>
                      </td></tr>';
   }
+  #d($xxxx);
 }
 
 ?>
