ff7e1cf90b082d7c64ad6f8884622ec777ac8601
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

--- language/english.php
+++ language/english.php
@@ -1,6 +1,6 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	Realm Status Portal Module
+ *	Package:	EQdkp-Plus Language File
  *	Link:		http://eqdkp-plus.eu
  *
  *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
@@ -19,30 +19,34 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if (!defined('EQDKP_INC')){
-	header('HTTP/1.0 404 Not Found');exit;
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-// Title
-$lang['realmstatus']				= 'Realmstatus';
-$lang['realmstatus_name']			= 'Realmstatus';
-$lang['realmstatus_desc']			= 'Display the current realm status';
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: portal/realmstatus/language/english.php
+//Source-Language: german
 
-//  Settings
-$lang['realmstatus_f_realm']		= 'List of servers';
-$lang['realmstatus_f_help_realm']	= 'For multiple servers the servers have to be insert comma separated';
-$lang['realmstatus_f_us']			= 'Is it an US server?';
-$lang['realmstatus_f_help_us']		= 'This setting has only effects if RIFT or WoW is set as game.';
-$lang['realmstatus_f_gd']			= 'GD Lib (%s) found. Do you want to use it?';
-$lang['realmstatus_f_help_gd']		= 'This setting only effects the game WoW.';
-
-// Portal Modul
-$lang['rs_no_realmname']			= 'No realm specified';
-$lang['rs_realm_not_found']			= 'Realm not found';
-$lang['rs_game_not_supported']		= 'Realmstatus is not supported for the current game';
-$lang['rs_unknown']					= 'Unknown';
-$lang['rs_realm_status_error']		= "Errors occured while determing realmstatus for %1\$s";
-$lang['rs_loading']					= 'Loading Status...';
-$lang['rs_loading_error']			= 'Failed to load Status.';
+$lang = array( 
+	"realmstatus" => 'Realmstatus',
+	"realmstatus_name" => 'Realmstatus',
+	"realmstatus_desc" => 'Display the current realm status',
+	"realmstatus_f_realm" => 'List of servers',
+	"realmstatus_f_help_realm" => 'For multiple servers the servers have to be insert comma separated',
+	"realmstatus_f_us" => 'Is it an US server?',
+	"realmstatus_f_help_us" => 'This setting has only effects if RIFT or WoW is set as game.',
+	"realmstatus_f_gd" => 'GD Lib (%s) found. Do you want to use it?',
+	"realmstatus_f_help_gd" => 'This setting only effects the game WoW.',
+	"rs_no_realmname" => 'No realm specified',
+	"rs_realm_not_found" => 'Realm not found',
+	"rs_game_not_supported" => 'Realmstatus is not supported for the current game',
+	"rs_unknown" => 'Unknown',
+	"rs_realm_status_error" => 'Errors occured while determing realmstatus for %1$s',
+	"rs_loading" => 'Loading Status...',
+	"rs_loading_error" => 'Failed to load Status.',
+	
+);
 
 ?>
