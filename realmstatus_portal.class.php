38a8e432cddc3484a6851b7ed91ad594c75c45cc
GodMod <GodMod@users.noreply.github.com>
2015-09-06 10:10:59 +0200

    Update realmstatus_portal.class.php

--- realmstatus_portal.class.php
+++ realmstatus_portal.class.php
@@ -31,7 +31,7 @@ class realmstatus_portal extends portal_generic {
 	protected static $path = 'realmstatus';
 	protected static $data = array(
 		'name'			=> 'Realmstatus Module',
-		'version'		=> '2.0.3',
+		'version'		=> '2.0.4',
 		'author'		=> 'Aderyn',
 		'icon'			=> 'fa-desktop',
 		'contact'		=> 'Aderyn@gmx.net',
